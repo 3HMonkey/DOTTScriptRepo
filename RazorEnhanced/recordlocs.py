0670673953ee232b987884fbe62a64bf82b78d32
Jean-Paul Sergent <zeropoint@macb0x.orsomething.local>
2020-04-29 15:32:45 -0700

    added better description to recordlocs.py

--- RazorEnhanced/recordlocs.py
+++ RazorEnhanced/recordlocs.py
@@ -1,3 +1,12 @@
-f = open("tree_locs_moon2.txt", "a")
+#Add this script to razorenhanced and bind it to a hot key.
+#You can then run the rail you want to record and press
+#the hotkey to record a point. This script is setup to
+#append to a file, so you can add to an existing file
+#if you start recording where you rail left off.
+#
+#name of rail file you want to write to
+file = "tree_locs_moon2.txt"
+
+f = open(file, "a")
 f.write(str(Player.Position) +"\n")
 f.close()
